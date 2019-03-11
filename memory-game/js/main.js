4e0f28b7f08e91862bb307b55d6642c293f17e07
Mujtaba Badar <mujbadar@macbook-pro.home>
2019-03-10 19:47:13 -0500

    create game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,17 @@
-console.log("Up and Running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king"
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+if(cardsInPlay.length === 2){
+alert("Sorry, try again.");
+} else if (cardsInPlay[0] === cardsInPlay[2]) {
+alert("You found a match!")
+}
+
+
+var cardOne=[0];
+cardsInPlay.push('cardOne');
+console.log("User flipped queen");
+
+var cardTwo=[2];
+cardsInPlay.push('cardTwo');
+console.log("User flipped king");
